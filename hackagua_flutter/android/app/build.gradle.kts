ae958c9ba1779dd1f72434dbcc7b685c11fa954f
Ana Beatriz Marques Moreira <anamarques@unitins.br>
2025-10-23 12:06:18 -0300

    tentativa de carregar o modelo

--- hackagua_flutter/android/app/build.gradle.kts
+++ hackagua_flutter/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.hackagua_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -45,4 +45,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("org.tensorflow:tensorflow-lite:2.16.1")
 }
